9a5b33aca20ff8ed21c978ef19cdabb6bc2d8cd2
Zzging <chanikan.po@mail.wu.ac.th>
2020-01-13 18:35:35 +0700

    add code class Salary

--- src/profit/Salary.java
+++ src/profit/Salary.java
@@ -1,5 +1,28 @@
 package profit;
 
 public class Salary {
-double salary;
+	 private double salary;
+	 private double commission;
+	
+	  public double getSalary() {
+	        return salary;
+	  }
+
+	   public void setSalary(double Salary) {
+	        this.salary = salary;
+	
+	  }
+	   public double getcommission() {
+	        return commission;
+	    }
+
+	    public void setcommission(double commission) {
+	        this.commission = commission;
+	    }  
+	    public void printSalary() {
+	    	  System.out.println("Salary:" + salary);
+	    	  System.out.println("Commission:" + commission);
+	    	  
+	        				
+	     }
 }
