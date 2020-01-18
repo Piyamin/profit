90e2c5dbd133741cb6eeb63488381bb508bff7b3
Zzging <chanikan.po@mail.wu.ac.th>
2020-01-18 15:34:52 +0700

    Change payment in Class Salary

--- src/profit/Salary.java
+++ src/profit/Salary.java
@@ -17,16 +17,16 @@ public class Salary {
 	  }
 
 	   public void CalPayment(double sales,double salary) {
-			   if (sales>50000)
+			   if (sales>100000)
 				  {
 				   commission = sales*(3.0/100);
 				   payment = salary + commission;
 			  } 
-			   else if (sales >25000){ 
+			   else if (sales >50000){ 
 				   commission = sales*(2.0/100);
 				   payment = salary + commission;
 				  }    
-			   else {
+			   else if(sales >0){
 				   commission = sales*(1.0/100);  
 				   payment = salary + commission;
 				   } 
